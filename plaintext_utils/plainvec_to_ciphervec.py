339c3af12a624d9cc094088050a5d759751e6146
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-11-30 03:41:53 +0700

    debig iteration

--- plaintext_utils/plainvec_to_ciphervec.py
+++ plaintext_utils/plainvec_to_ciphervec.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 
-def (plain_matrix, key, alpha_size):
-    cipher_matrix = key @ plain_matrix
+def transform(plain_matrix, key, alpha_size):
+    cipher_matrix = np.dot(key, plain_matrix)
 
-    for row in cipher_matrix.shape[0]:
-        for col in cipher_matrix.shape[1]:
+    for row in range(cipher_matrix.shape[0]):
+        for col in range(cipher_matrix.shape[1]):
             if cipher_matrix[row][col] >= alpha_size:
                 cipher_matrix[row][col] %= alpha_size
 
     return cipher_matrix
+
+
