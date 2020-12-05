6bd2c451b57bc95d3e5a9ee706e2bc3b90792a7c
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-06 01:23:54 +0700

    complete basic function

--- encipher.py
+++ encipher.py
@@ -8,30 +8,19 @@ Enciphers a plaintext.
 import numpy as np
 import scipy.linalg as lin
 import math as math
-import key_utils.length
-import key_utils.to_matrix
-import key_utils.invertible_matrix
-import vec_transformation_utils.str_to_vec
-import vec_transformation_utils.vec_to_vec
-import vec_transformation_utils.vec_to_str
-
-LET_MAP_NUM = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
-               "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22,
-               "W": 23, "X": 24, "Y": 25, "Z": 0}
-
-NUM_MAP_LET = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
-
-alpha_size = 26
+import key_utils
+import vec_transformation_utils
 
 
-def encipher(message, key, n):
+
+def encipher(message, key, n, alphabet):
     # step 2: process plaintext to numerical vector
-    P = vec_transformation_utils.str_to_vec.transform(message, n, LET_MAP_NUM)
+    P = vec_transformation_utils.str_to_vec(message, n, alphabet.let_map_num)
 
     # step 3: from plaintext vector to ciphertext vector
-    C = vec_transformation_utils.vec_to_vec.transform(P, key, alpha_size)
+    C = vec_transformation_utils.vec_to_vec(P, key, alphabet.alpha_size)
 
     # step 4: publish ciphertext
-    ciphertext = vec_transformation_utils.vec_to_str.transform(C, NUM_MAP_LET)
+    ciphertext = vec_transformation_utils.vec_to_str(C, alphabet.num_map_let)
 
     return ciphertext
