e709d7bd39520a321a70792b22f156b94a44cd45
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-06 01:24:07 +0700

    complete basic function

--- main.py
+++ main.py
@@ -4,50 +4,50 @@ import math as math
 
 import decipher
 import encipher
-import key_utils.length
-import key_utils.to_matrix
-import key_utils.invertible_matrix
-import vec_transformation_utils.str_to_vec
-import vec_transformation_utils.vec_to_vec
+import key_utils
 
-ALPHABET_ID = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
-                "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22,
-                "W": 23, "X": 24, "Y": 25, "Z": 0}
+import vec_transformation_utils
+import vec_transformation_utils
+from alphabet_utils.alphabet import Alphabet
 
-CIPHER_ID = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
 
-alpha_size = 26
+alphabet = Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 #step 1:
+print("\nConsole Version")
+print ("\n***WELCOME TO BIJOU & CHRISTINE CRYPTOLAND***\n")
 
 #Client Input: message, password
+action = input("encode or decode? \n")
 
-message = input ("Coded message: ")
+message = input ("\nmessage:\n")
 
-type_password = input ("numerical or alphabet?")
+type_password = input ("\nPassword type: num or alp? \n")
 
-password = input("Password: ")
+password = input("\nPassword: \n")
 
-action = input("encode or decode? ")
+print()
+
+
 
 #check valid key
-valid, n = key_utils.length.has_valid_length(len(password))
+valid, n = key_utils.has_valid_length(len(password))
 if not valid:
     print ("Error: length is not a perfect square 4, 9, 16, ... ")
 
 else:
-    key = key_utils.to_matrix.to_matrix(password, n, type_password)
-    if key_utils.invertible_matrix.is_invertible(key):
+    key = key_utils.to_matrix(password, n, type_password,alphabet)
+    if key_utils.is_invertible(key, alphabet):
         if action == 'encode':
             print("Your message is being encoded")
-            print(encipher.encipher(message, key, n))
+            print(encipher.encipher(message, key, n, alphabet))
 
         else:
             if len(message) % n != 0:
                 print('Error: length of message must be multiple of n')
 
             else:
-                print("Your message is being encoded")
-                print(decipher.decipher(message, key, n))
+                print("Your message is being decoded")
+                print(decipher.decipher(message, key, n, alphabet))
 
     else:
         print('Please change last 2 characters')
