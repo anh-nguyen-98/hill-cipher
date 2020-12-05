451fab84a589bccb2a2e16648183c6ed27fd696f
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-06 01:26:30 +0700

    deleted files

--- vec_transformation_utils/vec_to_str.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def transform(matrix, num_map_let):
-    text = ""
-    for col in range(matrix.shape[1]):
-        for row in range(matrix.shape[0]):
-            text += num_map_let[matrix[row][col]]
-
-    return text
