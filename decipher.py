b0cd81e3e0f544c6df868032c4c071c4c612373d
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-06 01:23:39 +0700

    complete basic function

--- decipher.py
+++ decipher.py
@@ -9,40 +9,27 @@ Decodes a ciphertext.
 import numpy as np
 import scipy.linalg as lin
 import math as math
-import key_utils.length
-import key_utils.to_matrix
-import key_utils.invertible_matrix
-import vec_transformation_utils.str_to_vec
-import vec_transformation_utils.vec_to_vec
-import vec_transformation_utils.vec_to_str
+import key_utils
+import vec_transformation_utils
+from inverse_matrix_modulo import inv
 
-LET_MAP_NUM = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
-               "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22,
-               "W": 23, "X": 24, "Y": 25, "Z": 0}
 
-NUM_MAP_LET = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
 
-alpha_size = 26
-
-def decipher(message, key, n):
+def decipher(message, key, n, alphabet):
     #ciphermatrix
     # step 2: process ciphertext to numerical vector
-    C = vec_transformation_utils.str_to_vec.transform(message, n, LET_MAP_NUM)
+    C = vec_transformation_utils.str_to_vec(message, n, alphabet.let_map_num)
 
-    # step 3: from plaintext vector to ciphertext vector
+    # step 3: from cipher vector to plaintext vector
 
-    #finds inverse key mod n (deciphering matrix)
-        # finds determinant of key
+        #finds inverse key mod n (deciphering matrix)
+    key_inverse = inv(key, alphabet.alpha_size)
 
+        # deciphering_matrix @ cipher_matrix
+    P = vec_transformation_utils.vec_to_vec(C, key_inverse, alphabet.alpha_size)
 
-
-        # finds adjugate matrix of key
-
-    # step 4: deciphering_matrix @ cipher_matrix
-    P = vec_transformation_utils.vec_to_vec.transform(C, key, alpha_size)
-
-    # step 5: publish ciphertext
-    plaintext = vec_transformation_utils.vec_to_str(P, NUM_MAP_LET)
+    # step 4: publish plaintext
+    plaintext = vec_transformation_utils.vec_to_str(P, alphabet.num_map_let)
 
     return plaintext
 
